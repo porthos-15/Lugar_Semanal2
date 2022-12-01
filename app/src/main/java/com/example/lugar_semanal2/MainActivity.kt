aa436641f1bffe153aab47313d4bf97f9c5e9cf9
porthos-15 <kevinesquivel115@gmail.com>
2022-12-01 09:31:44 -0600

    Initial commit semana 1

---
+++ app/src/main/java/com/example/lugar_semanal2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lugar_semanal2
+
+
+
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    //Definimos un objeto para acceder a la autenticacion de firebase
+
+}
